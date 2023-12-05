ed4a7443b6858e3f45338f315683d2eb2d6dd9e4
pratikshavilastake <pratikshatake131@gmail.com>
2023-12-05 17:51:42 +0530

    Initial commit for this project

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,6 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Demo2 {
     public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
 
-        System.out.println("Hello and WORLD!");
-    }
+           System.setProperty("webdriver.chrome.driver", "/Users/pratikshavilastake/Documents/CHdriver/chromedriver");
+
+             WebDriver driver = new ChromeDriver();
+             // Navigate to the desired URL
+            driver.get("https://www.naukri.com/");
+
+        }
 }
+
+
